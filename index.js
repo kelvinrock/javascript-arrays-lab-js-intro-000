48bc9e3ffbcb212e05c1521c8e8b1027b443c40d
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-10 12:53:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
-  return name.push["Ralph"]
+  kittens.push(name)
+  return kittens
 }
